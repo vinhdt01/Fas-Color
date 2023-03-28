43eabc8c47a3a6d7b914ba0f61ee292ec94fcb5b
Đào Thế Vinh <57987212+vinhdt01@users.noreply.github.com>
2023-03-28 23:07:13 +0700

    Update get-one-product.ts

--- src/framework/rest/ssr/get-one-product.ts
+++ src/framework/rest/ssr/get-one-product.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export async function getServerSideProps(context) {
-    let slug = context.params.slug
-    let base_url = process.env.BASE_URL;
-    ;
- const res = await axios.get(`${base_url}` + '/get-product/' +     `${slug}`);
-  const data = res.data;
+//     let slug = context.params.slug
+//     let base_url = process.env.BASE_URL;
+//     ;
+//  const res = await axios.get(`${base_url}` + '/get-product/' +     `${slug}`);
+//   const data = res.data;
 
   return {
     props: {
-      data,
+      data:'dasdsa',
     },
   };
 }
