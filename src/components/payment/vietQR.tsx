907c88611e94298c09eef3aad9e8722e6fba10bc
Đào Thế Vinh <57987212+vinhdt01@users.noreply.github.com>
2023-05-02 23:05:16 +0700

    up

--- src/components/payment/vietQR.tsx
+++ src/components/payment/vietQR.tsx
@@ -32,7 +32,7 @@ export default function VietQR({handleSetShow}:any) {
     <div className="fixed top-0 right-0 left-0 bottom-0  z-50  ">
      
      <div className="m-auto mt-[45px] flex flex-col items-center bg-white  max-w-[800px] p-[15px] rounded-lg">
-         <img  className="m-auto bg-white" src={`https://img.vietqr.io/image/vietinbank-113366668888-compact2.jpg?amount=${total}&addInfo=dong%20qop%20quy%20vac%20xin&accountName=Thanh toán đơn hàng`}/>
+         <img  className="m-auto bg-white" src={`https://img.vietqr.io/image/vietinbank-0702686901-compact2.jpg?amount=${total}&addInfo=dong%20qop%20quy%20vac%20xin&accountName=Thanh toán đơn hàng`}/>
          <p onClick={handleSetVietQR} className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">    
            Thanh toán xong 
          </p>
