fe1c0a6fbe172463e50adadeef8d47b6b27e463a
Đào Thế Vinh <57987212+vinhdt01@users.noreply.github.com>
2023-05-08 21:23:17 +0700

    up

--- analytics.ts
+++ analytics.ts
@@ -1,10 +1,10 @@
 import ReactGA from 'react-ga';
 
-export const initGA = () => {
-  ReactGA.initialize('UA-XXXXXXXXX-X'); // Thay thế UA-XXXXXXXXX-X bằng mã theo dõi của bạn
-}
+export const initGA = (trackingID: string) => {
+  ReactGA.initialize(trackingID);
+};
 
 export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
-}
+};
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,22 +1,16 @@
-import { useEffect } from 'react'
 import Header from "@components/layout/header/header";
 import Footer from "@components/layout/footer/footer";
 import MobileNavigation from "@components/layout/mobile-navigation/mobile-navigation";
 import Search from "@components/common/search";
 import React from "react";
 import ChatGPT from '@components/chat/chatgpt';
-import ReactGA from 'react-ga';
-export const initGA = (trackingID) => {
-	ReactGA.initialize(trackingID);
-  };
-  export const logPageView = () => {
-	ReactGA.set({ page: window.location.pathname });
-	ReactGA.pageview(window.location.pathname);
-  };
+import { initGA, logPageView } from '../../../analytics';
+import { useEffect } from 'react';
+
 const SiteLayout: React.FC = ({ children }) => {
 	useEffect(() => {
 		if (!window.GA_INITIALIZED) {
-		  initGA(361834544); // Thay YOUR_TRACKING_ID bằng mã theo dõi (tracking ID) của bạn
+		  initGA('361834544'); // Thay YOUR_TRACKING_ID bằng mã theo dõi (tracking ID) của bạn
 		  window.GA_INITIALIZED = true;
 		}
 		logPageView();
