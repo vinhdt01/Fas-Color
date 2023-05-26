7f4da449f888b2e985984a5035780aed602592e2
Đào Thế Vinh <57987212+vinhdt01@users.noreply.github.com>
2023-05-26 10:41:19 +0700

    up

--- src/components/chat/chatgpt-conversation.tsx
+++ src/components/chat/chatgpt-conversation.tsx
@@ -7,6 +7,10 @@ const ChatGPTConversation = () => {
     const [chatLog , setChatLog] = useState<any>([])
     const [loading , setLoading] = useState<boolean>(false)
     const bottomRef = useRef<any>(null);
+    const handleSubmit = (e:any) => {
+      e.preventDefault();
+      chat()
+    }
 
    const chat = () => {
     let data = JSON.stringify({
@@ -87,12 +91,12 @@ const ChatGPTConversation = () => {
                 </div>
                <div/>
                </div>
-               <div  className="w-full flex items-center mb-[10px] bg-[#f7eded] rounded-b-lg py-[10px] px-[10px]">
+               <form onSubmit={handleSubmit}  className="w-full flex items-center mb-[10px] bg-[#f7eded] rounded-b-lg py-[10px] px-[10px]">
                     <input value={inputValue} onChange={e=> setInputValue(e.target.value)} className="w-full mr-[10px] outline-none border-2 h-[40px] rounded-full px-[5px] placeholder-gray-500 placeholder-opacity-50 placeholder:text[12px] placeholder:italic " type="text" placeholder="Chúng tôi có thể giúp gì cho bạn ?"/>
-                    <button onClick={chat} className="h-[40px] w-[40px] bg-[#5d5cde] rounded-full p-[5px]">
+                    <button type="submit" className="h-[40px] w-[40px] bg-[#5d5cde] rounded-full p-[5px]">
                     <svg fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 13h14.09l-6.79 6.79a.996.996 0 1 0 1.41 1.41l8.5-8.5c.06-.06.09-.13.13-.2.03-.04.06-.08.08-.13a.91.91 0 0 0 .08-.37c0-.03-.01-.05-.01-.07-.01-.1-.02-.21-.06-.31a.955.955 0 0 0-.22-.33L12.72 2.8c-.2-.2-.45-.29-.71-.29-.26 0-.51.1-.71.29a.996.996 0 0 0 0 1.41L18.08 11H4c-.55 0-1 .45-1 1s.45 1 1 1Z"></path></svg>
                     </button>
-                </div>
+                </form>
            </div>
 
 
